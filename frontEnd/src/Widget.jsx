9bb4abbecf3a3707e986b69559cb78877b2854aa
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-02-18 01:00:43 -0300

    start add Service

--- frontEnd/src/Widget.jsx
+++ frontEnd/src/Widget.jsx
@@ -4,166 +4,68 @@ import './widget.css';
 
 
 const Widget = () => {
-  const [mostrarEndereco, setMostrarEndereco] = useState(false);
-  const [messageEndereco, setMessageEndereco] = useState('');
-  const [endereco, setEndereco] = useState('');
-const barbeariaId = 1;
-  //Função para mostrar os inputs de alteração de endereço
-  const alternarEndereco = () => {
-    setMostrarEndereco(!mostrarEndereco);
-  };
-  //Obtendo os valores dos inputs
-  const [valuesEndereco, setValuesEndereco] = useState({
-    street: '',
-    number:'',
-    neighborhood:'',
-    city:''
-  });
-  //Função para vericicar se há algum input vazio
-  const verificarValoresPreenchidos = () => {
-    for (const key in valuesEndereco) {
-      if (valuesEndereco.hasOwnProperty(key) && !valuesEndereco[key]) {
-        return false; // Retorna falso se algum valor não estiver preenchido
-      }
-    }
-    return true; // Retorna verdadeiro se todos os valores estiverem preenchidos
-  };
-  console.log(valuesEndereco)
-  //Função responsável por enviar os valores ao back-end
-  const alterarEndereco = () => {
-    if (verificarValoresPreenchidos()) {
-      axios.post(`https://api-user-barbeasy.up.railway.app/api/update-endereco/${barbeariaId}`, { Values: valuesEndereco })
-        .then(res => {
-          if (res.data.Success === 'Success') {
-            setMessageEndereco("Endereço Alterado com Sucesso!")
-            // Limpar a mensagem após 3 segundos (3000 milissegundos)
-            setTimeout(() => {
-              setMessageEndereco('');
-              window.location.reload();
-            }, 3000);
-          }
-        })
-        .catch(error => {
-          setMessageEndereco('Erro ao atualizar o endereço.');
-          // Limpar a mensagem de erro após 3 segundos (3000 milissegundos)
-          setTimeout(() => {
-            setMessageEndereco('');
-          }, 3000);
-          // Lógica a ser executada em caso de erro na solicitação
-          console.error('Erro ao atualizar o endereço da barbearia:', error);
-        });
-    } else {
-      setMessageEndereco('Altere todos os campos de endereço.');
+  const barbeariaId = 1;
 
-      setTimeout(() => {
-        setMessageEndereco('');
-      }, 3000);
-    }
-  };
-  //Função para obter o nome atual da barbearia
-  useEffect(() => {
-    axios.get(`https://api-user-barbeasy.up.railway.app/api/endereco/${barbeariaId}`)
-      .then(res => {
-        setEndereco(res.data.Endereco)
-      })
-      .catch(error => console.log(error));
-  }, [barbeariaId])
+  const [mostrarServico, setMostrarServico] = useState(false);
+  const [showAddServico, setShowAddServico] = useState(false);
 
-/*-------------------------------------------*/
+  const alternarServico = () => {
+    setMostrarServico(!mostrarServico);
+  };
+
+  const alternarAddServico = () => {
+    setShowAddServico(true);
+  };
+
   return (
     <>
-    <div className="menu__main" onClick={alternarEndereco} >
-            <span className="material-symbols-outlined icon_menu">pin_drop</span>
-              Endereço
-              <span className={`material-symbols-outlined arrow ${mostrarEndereco ? 'girar' : ''}`} id='arrow'>expand_more</span>
+      <div className="menu__main" onClick={alternarServico}>
+        <span className="material-symbols-outlined icon_menu">cut</span>
+        Definir Serviços
+        <span className={`material-symbols-outlined arrow ${mostrarServico ? 'girar' : ''}`} id='arrow'>expand_more</span>
+      </div>
+
+      {mostrarServico && (
+        <div className="divSelected">
+
+          <div className='container__servicos'>
+            
+          </div>
+
+          <div className={`add_Service ${showAddServico ? 'expandir' : ''}`} onClick={alternarAddServico}>
+            {showAddServico &&(
+              <div className="input_Container">
+                    <p className='information__span'>Qual o nome do serviço?</p>
+                    <input className="input_AddService" type="text" id="serviceName" name="serviceName" placeholder='Ex. Corte Social'/>
+                    <p className='information__span'>Quanto vai custar?</p>
+                    <input className="input_AddService" type="text" id="serviceName" name="serviceName" placeholder='R$ 00,00'/>
+                    <p className='information__span'>Qual o tempo de duração?</p>
+                    <div className="inputs-horarios">
+                      {['15min','30min','45min','60min','75min', '90min'].map((atendimento, index) => (
+                        <div
+                          key={index}
+                          className={`horario-item ${showAddServico ? 'Horario-selecionado' : ''}`}
+                        >
+                          <p>{atendimento}</p>
+                        </div>
+                      ))}
+                    </div>
+
+                    <button className="add_Service" onChange={() => setShowAddServico(!showAddServico)}>
+                      Adicionar Serviço
+                    </button>
+
+                
+                
+              </div>
+            )}
+           
+              <span className="material-symbols-outlined">add</span>
+              Adicionar Serviço
+              
+          </div>
         </div>
-
-        {mostrarEndereco && (
-                    <div className="divSelected">
-                      <p className='information__span'>Altere o endereço da Barbearia</p>
-
-                      {messageEndereco === 'Endereço Alterado com Sucesso!' ?
-                        <p className="mensagem-sucesso">{messageEndereco}</p>
-                        :
-                        <p className="mensagem-erro">{messageEndereco}</p>
-                      }
-                      
-                      <div className="inputBox">
-                        <input
-                        type="text"
-                        id="street"
-                        name="street"
-                        onChange={(e) => {
-                          const inputValue = e.target.value;
-                          // Remover caracteres especiais
-                          const sanitizedValue = inputValue.replace(/[^a-zA-Z0-9\sçéúíóáõãèòìàêôâ]/g, '');
-
-                          // Limitar a 50 caracteres
-                          const truncatedValue = sanitizedValue.slice(0, 50);
-                          setValuesEndereco({ ...valuesEndereco, street: truncatedValue });
-                        }}
-                        placeholder={endereco[0]}
-                        className="white-placeholder"
-                        required
-                      /> <span className="material-symbols-outlined icon_input">add_road</span>
-
-                    <input
-                      type="text"
-                      id="number"
-                      name="number"
-                      onChange={(e) => {
-                        const inputValue = e.target.value;
-                        // Remover caracteres não numéricos
-                        const numericValue = inputValue.replace(/\D/g, '');
-                        // Limitar a 10 caracteres
-                        const truncatedValue = numericValue.slice(0, 5);
-                        setValuesEndereco({ ...valuesEndereco, number: truncatedValue });
-                      }}
-                      placeholder={endereco[1]}
-                      className="white-placeholder"
-                      required
-                    />{' '} <span className="material-symbols-outlined" id="icon_street_number">home_pin</span>
-                    
-                    <input
-                      type="text"
-                      id="neighborhood"
-                      name="neighborhood"
-                      onChange={(e) => {
-                        const inputValue = e.target.value;
-                        // Remover caracteres especiais
-                        const sanitizedValue = inputValue.replace(/[^a-zA-Z0-9\sçéúíóáõãèòìàêôâ]/g, '');
-                        // Limitar a 50 caracteres
-                        const truncatedValue = sanitizedValue.slice(0, 50);
-                        setValuesEndereco({ ...valuesEndereco, neighborhood: truncatedValue });
-                      }}
-                      placeholder={endereco[2]}
-                      className="white-placeholder"
-                      required
-                    /><span className="material-symbols-outlined" id="icon_input_neighborhood">route</span>
-                    
-                    <input
-                      type="text"
-                      id="city"
-                      name="city"
-                      onChange={(e) => {
-                        const inputValue = e.target.value;
-                        // Remover caracteres especiais
-                        const sanitizedValue = inputValue.replace(/[^a-zA-Z0-9\sçéúíóáõãèòìàêôâ]/g, '');
-                        // Limitar a 50 caracteres
-                        const truncatedValue = sanitizedValue.slice(0, 30);
-                        setValuesEndereco({ ...valuesEndereco, city: truncatedValue });
-                      }}
-                      placeholder={endereco[3]}
-                      className="white-placeholder"
-                      required
-                    />{' '} <span className="material-symbols-outlined" id="icon_input_city">map</span>
-                      </div>
-
-                      <button className={`button__change ${valuesEndereco.city ? 'show' : ''}`} onClick={alterarEndereco}>
-                        Alterar
-                      </button>
-                    </div>          
-        )}
+      )}
     </>
   );
 };
