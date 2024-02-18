471f12e3ee57f92e53d14b9ca342a021f0697a6d
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-02-17 23:17:56 -0300

    Update Widget.jsx

--- frontEnd/src/Widget.jsx
+++ frontEnd/src/Widget.jsx
@@ -4,90 +4,166 @@ import './widget.css';
 
 
 const Widget = () => {
-  const [mostrarNome, setMostrarNome] = useState(false);
-  const [novoUserName, setNovoUserName] = useState('');
-  const [userNameBarbearia, setUserNameBarbearia] = useState('');
-  const [messageUserName, setMessageUserName] = useState('');
-
-  const barbeariaId = 1;
-
-  const alternarNome = () => {
-      setMostrarNome(!mostrarNome);
+  const [mostrarEndereco, setMostrarEndereco] = useState(false);
+  const [messageEndereco, setMessageEndereco] = useState('');
+  const [endereco, setEndereco] = useState('');
+const barbeariaId = 1;
+  //Função para mostrar os inputs de alteração de endereço
+  const alternarEndereco = () => {
+    setMostrarEndereco(!mostrarEndereco);
   };
-  //Função responsável por enviar o novo nome de usuário ao back-end
-  const alterarUserName = () => {
-    axios.post(`https://api-user-barbeasy.up.railway.app/api/upload-user-name-barbearia/${barbeariaId}`, {newUserName: novoUserName})
-      .then(res => {
-          if(res.data.Success === 'Success'){
-            setMessageUserName("Nome de Usuário Alterado com Sucesso!")
+  //Obtendo os valores dos inputs
+  const [valuesEndereco, setValuesEndereco] = useState({
+    street: '',
+    number:'',
+    neighborhood:'',
+    city:''
+  });
+  //Função para vericicar se há algum input vazio
+  const verificarValoresPreenchidos = () => {
+    for (const key in valuesEndereco) {
+      if (valuesEndereco.hasOwnProperty(key) && !valuesEndereco[key]) {
+        return false; // Retorna falso se algum valor não estiver preenchido
+      }
+    }
+    return true; // Retorna verdadeiro se todos os valores estiverem preenchidos
+  };
+  console.log(valuesEndereco)
+  //Função responsável por enviar os valores ao back-end
+  const alterarEndereco = () => {
+    if (verificarValoresPreenchidos()) {
+      axios.post(`https://api-user-barbeasy.up.railway.app/api/update-endereco/${barbeariaId}`, { Values: valuesEndereco })
+        .then(res => {
+          if (res.data.Success === 'Success') {
+            setMessageEndereco("Endereço Alterado com Sucesso!")
             // Limpar a mensagem após 3 segundos (3000 milissegundos)
             setTimeout(() => {
-              setMessageUserName('');
-              
+              setMessageEndereco('');
+              window.location.reload();
             }, 3000);
           }
         })
         .catch(error => {
-          setMessageUserName("Erro ao atualizar o nome de usuário.")
-            // Limpar a mensagem após 3 segundos (3000 milissegundos)
-            setTimeout(() => {
-              setMessageUserName('');
-              
-            }, 3000);
-          console.error('Erro ao atualizar o nome de usuário:', error);
+          setMessageEndereco('Erro ao atualizar o endereço.');
+          // Limpar a mensagem de erro após 3 segundos (3000 milissegundos)
+          setTimeout(() => {
+            setMessageEndereco('');
+          }, 3000);
+          // Lógica a ser executada em caso de erro na solicitação
+          console.error('Erro ao atualizar o endereço da barbearia:', error);
         });
+    } else {
+      setMessageEndereco('Altere todos os campos de endereço.');
+
+      setTimeout(() => {
+        setMessageEndereco('');
+      }, 3000);
+    }
   };
-  //Função para obter o nome de usuário atual da barbearia
+  //Função para obter o nome atual da barbearia
   useEffect(() => {
-    axios.get(`https://api-user-barbeasy.up.railway.app/api/user-name-barbearia/${barbeariaId}`)
+    axios.get(`https://api-user-barbeasy.up.railway.app/api/endereco/${barbeariaId}`)
       .then(res => {
-        setUserNameBarbearia(res.data.UserNameBarbearia)
+        setEndereco(res.data.Endereco)
       })
       .catch(error => console.log(error));
   }, [barbeariaId])
 
-console.log(novoUserName)
-console.log(userNameBarbearia)
-
 /*-------------------------------------------*/
   return (
     <>
-    <div className="menu__main" onClick={alternarNome}>
-          <span className="material-symbols-outlined icon_menu">person</span>
-            Nome
-            <span className={`material-symbols-outlined arrow ${mostrarNome ? 'girar' : ''}`} id='arrow'>expand_more</span>
-          </div>
-      {mostrarNome && (
-            <div className="divSelected">
-              <p className='information__span'>Alterar Nome de usuário</p>
-              <p className="mensagem-sucesso">{messageUserName}</p>
+    <div className="menu__main" onClick={alternarEndereco} >
+            <span className="material-symbols-outlined icon_menu">pin_drop</span>
+              Endereço
+              <span className={`material-symbols-outlined arrow ${mostrarEndereco ? 'girar' : ''}`} id='arrow'>expand_more</span>
+        </div>
 
-            <div className="inputBox">
-            <input
-                type="text"
-                id="usuario"
-                name="usuario"
-                maxLength={30}
-                onChange={(e) => {
-                  const inputValue = e.target.value;
-                  // Remover caracteres não alfanuméricos
-                  const filteredValue = inputValue.replace(/[^a-zA-Z0-9.\s]/g, '');
-                  // Limitar a 30 caracteres
-                  const userName = filteredValue.slice(0, 30);
-                setNovoUserName(userName);
-                }}
-                placeholder={userNameBarbearia}
-                className="white-placeholder"
-                required
-              />{' '}<span className="material-symbols-outlined icon_input">person_edit</span>
-            </div>
+        {mostrarEndereco && (
+                    <div className="divSelected">
+                      <p className='information__span'>Altere o endereço da Barbearia</p>
 
-            <button className={`button__change ${novoUserName ? 'show' : ''}`} onClick={alterarUserName}>
-              Alterar
-            </button>
-         </div>
-         
-          )}
+                      {messageEndereco === 'Endereço Alterado com Sucesso!' ?
+                        <p className="mensagem-sucesso">{messageEndereco}</p>
+                        :
+                        <p className="mensagem-erro">{messageEndereco}</p>
+                      }
+                      
+                      <div className="inputBox">
+                        <input
+                        type="text"
+                        id="street"
+                        name="street"
+                        onChange={(e) => {
+                          const inputValue = e.target.value;
+                          // Remover caracteres especiais
+                          const sanitizedValue = inputValue.replace(/[^a-zA-Z0-9\sçéúíóáõãèòìàêôâ]/g, '');
+
+                          // Limitar a 50 caracteres
+                          const truncatedValue = sanitizedValue.slice(0, 50);
+                          setValuesEndereco({ ...valuesEndereco, street: truncatedValue });
+                        }}
+                        placeholder={endereco[0]}
+                        className="white-placeholder"
+                        required
+                      /> <span className="material-symbols-outlined icon_input">add_road</span>
+
+                    <input
+                      type="text"
+                      id="number"
+                      name="number"
+                      onChange={(e) => {
+                        const inputValue = e.target.value;
+                        // Remover caracteres não numéricos
+                        const numericValue = inputValue.replace(/\D/g, '');
+                        // Limitar a 10 caracteres
+                        const truncatedValue = numericValue.slice(0, 5);
+                        setValuesEndereco({ ...valuesEndereco, number: truncatedValue });
+                      }}
+                      placeholder={endereco[1]}
+                      className="white-placeholder"
+                      required
+                    />{' '} <span className="material-symbols-outlined" id="icon_street_number">home_pin</span>
+                    
+                    <input
+                      type="text"
+                      id="neighborhood"
+                      name="neighborhood"
+                      onChange={(e) => {
+                        const inputValue = e.target.value;
+                        // Remover caracteres especiais
+                        const sanitizedValue = inputValue.replace(/[^a-zA-Z0-9\sçéúíóáõãèòìàêôâ]/g, '');
+                        // Limitar a 50 caracteres
+                        const truncatedValue = sanitizedValue.slice(0, 50);
+                        setValuesEndereco({ ...valuesEndereco, neighborhood: truncatedValue });
+                      }}
+                      placeholder={endereco[2]}
+                      className="white-placeholder"
+                      required
+                    /><span className="material-symbols-outlined" id="icon_input_neighborhood">route</span>
+                    
+                    <input
+                      type="text"
+                      id="city"
+                      name="city"
+                      onChange={(e) => {
+                        const inputValue = e.target.value;
+                        // Remover caracteres especiais
+                        const sanitizedValue = inputValue.replace(/[^a-zA-Z0-9\sçéúíóáõãèòìàêôâ]/g, '');
+                        // Limitar a 50 caracteres
+                        const truncatedValue = sanitizedValue.slice(0, 30);
+                        setValuesEndereco({ ...valuesEndereco, city: truncatedValue });
+                      }}
+                      placeholder={endereco[3]}
+                      className="white-placeholder"
+                      required
+                    />{' '} <span className="material-symbols-outlined" id="icon_input_city">map</span>
+                      </div>
+
+                      <button className={`button__change ${valuesEndereco.city ? 'show' : ''}`} onClick={alterarEndereco}>
+                        Alterar
+                      </button>
+                    </div>          
+        )}
     </>
   );
 };
