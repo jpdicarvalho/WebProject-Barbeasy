0ae1107a229998cc34c94be038dff35188538349
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-02-22 20:49:27 -0300

    update module add service

--- frontEnd/src/Widget.jsx
+++ frontEnd/src/Widget.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import './widget.css';
 
 
@@ -8,6 +7,10 @@ const Widget = () => {
 
   const [mostrarServico, setMostrarServico] = useState(false);
   const [showAddServico, setShowAddServico] = useState(false);
+  const [nomeServiço, setNomeServiço] = useState('');
+  const [precoServiço, setPrecoServiço] = useState('');
+  const [messageAddService, setMessageAddService] = useState('');
+
 
   const alternarServico = () => {
     setMostrarServico(!mostrarServico);
@@ -17,12 +20,38 @@ const Widget = () => {
     setShowAddServico(true);
   };
 
+  const formatarPreco = (valor) => {
+    const numero = valor.replace(/\D/g, ''); // Remove caracteres não numéricos
+    const valorFormatado = (Number(numero) / 100).toFixed(2).replace('.', ',').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+    return `R$ ${valorFormatado}`;
+  };
+
+  const handleChangePreco = (event) => {
+    const valor = event.target.value;
+    // Filtrar apenas os números
+    const numero = valor.replace(/\D/g, '');
+    setPrecoServiço(formatarPreco(numero));
+  };
+
+  const adicionarServico = () => {
+    setMessageAddService("SERVICE ADD SUCCESSFULLY");
+    setTimeout(() => {
+      setMessageAddService(null);
+      setShowAddServico(false);
+    }, 3000);
+    
+    // // Fechar a div input_Container
+  };
+
+  console.log(nomeServiço)
+  console.log(precoServiço)
+
   return (
     <>
       <div className="menu__main" onClick={alternarServico}>
-        <span className="material-symbols-outlined icon_menu">cut</span>
+        <span className="material-symbols-outlined icon_menu"/>
         Definir Serviços
-        <span className={`material-symbols-outlined arrow ${mostrarServico ? 'girar' : ''}`} id='arrow'>expand_more</span>
+        <span className={`material-symbols-outlined arrow ${mostrarServico ? 'girar' : ''}`} id='arrow'/>
       </div>
 
       {mostrarServico && (
@@ -36,9 +65,30 @@ const Widget = () => {
             {showAddServico &&(
               <div className="input_Container">
                     <p className='information__span'>Qual o nome do serviço?</p>
-                    <input className="input_AddService" type="text" id="serviceName" name="serviceName" placeholder='Ex. Corte Social'/>
+
+                    <input
+                    className="input_AddService"
+                    type="text"
+                    id="serviceName"
+                    name="serviceName"
+                    maxLength={30}
+                    onChange={e => setNomeServiço(e.target.value)}
+                    placeholder='Ex. Corte Social'
+                    />
+
                     <p className='information__span'>Quanto vai custar?</p>
-                    <input className="input_AddService" type="text" id="serviceName" name="serviceName" placeholder='R$ 00,00'/>
+                    <input
+                    className="input_AddService"
+                    type="text"
+                    id="precoServico"
+                    name="precoServico"
+                    value={precoServiço}
+                    onChange={handleChangePreco}
+                    maxLength={9}
+                    placeholder="R$ 00,00"
+                    required
+                  />
+
                     <p className='information__span'>Qual o tempo de duração?</p>
                     <div className="inputs-horarios">
                       {['15min','30min','45min','60min','75min', '90min'].map((atendimento, index) => (
@@ -50,13 +100,11 @@ const Widget = () => {
                         </div>
                       ))}
                     </div>
+                    {messageAddService}
 
-                    <button className="add_Service" onChange={() => setShowAddServico(!showAddServico)}>
+                    <button className="add_Service" onClick={adicionarServico}>
                       Adicionar Serviço
                     </button>
-
-                
-                
               </div>
             )}
            
